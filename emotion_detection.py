07f18c332fd13ffa413f5c66bd26cd4ae1582526
Erika <erikaa@mtu.edu>
2025-07-26 10:58:26 -0400

    change

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,15 +3,20 @@ import json
 
 def emotion_detector(text_to_analyze):
     print("Text to analyze:", text_to_analyze)
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
     json_data = {
         "raw_document": { 
             "text": text_to_analyze 
             }
         }
+    
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     print("Sending request...")
+
     try:
-        req = requests.post('https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict', headers=headers, json=json.dumps(json_data), timeout=10)
+        req = requests.post(url=url, headers=headers, json=json.dumps(json_data), timeout=10)
         return req.text
     except Exception as e: 
         print("Request failed")
